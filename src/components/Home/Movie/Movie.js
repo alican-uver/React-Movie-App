bde06b3fb344a32bf81e40aa18e9a563d905347a
Alican Uver <can.uverr@gmail.com>
2020-04-09 03:34:00 +0300

    Turkish language option was introduced. If not in Turkish, information will be displayed in English

--- src/components/Home/Movie/Movie.js
+++ src/components/Home/Movie/Movie.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BASE_URL,API_KEY } from '../../../config';
+import { BASE_URL, API_KEY } from '../../../config';
 import Spinner from '../Spinner/Spinner';
 import MovieInfo from '../MovieInfo/MovieInfo';
 
@@ -13,57 +13,99 @@ class Movie extends Component {
         directors: []
     }
 
-    
     componentDidMount() {
         console.log(this.props);
-        let endPoint = `${BASE_URL}/movie/${this.props.match.params.movieId}?api_key=${API_KEY}`;
-        this.getMovieWithId(endPoint)
+        let endPoint = `${BASE_URL}/movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=tr`
+        this.getMovieWithId(endPoint);
+        this.getDirectorsAndActors();
     }
-    
-    getMovieWithId = endPoint => {
-        fetch(endPoint)
-        .then(response => response.json())
-        .then((movie) => {
-            console.log(movie)
 
-            if (movie.status_code) { // For error the request
-                this.setState({
-                    loader: false
-                })
-            }
-            else { // İf success the request
-                this.setState({ movie }, () => {
-                    let endPoint = `${BASE_URL}/movie/${this.props.match.params.movieId}/credits?api_key=${API_KEY}`;
-                    fetch(endPoint)
+    getMovieWithId = (endPoint) => {
+        fetch(endPoint)
+            .then(response => response.json())
+            .then((movie) => {
+                console.log(movie);
+                
+                if (movie.overview !== "") {
+                    this.setState({
+                        movie
+                    })
+                }
+                else {
+                    let engEndPoint = `${BASE_URL}/movie/${this.props.match.params.movieId}?api_key=${API_KEY}`
+                    fetch(engEndPoint)
                     .then(response => response.json())
-                    .then((credits) => {
-                        console.log(credits)
-                        const filterDirector = credits.crew.filter(person => person.job === "Director"); // filter directors from all employees
-                        console.log(filterDirector)
+                    .then((movie) => {
                         this.setState({
-                            actors: credits.cast,
-                            directors: filterDirector[0].name,
-                            loader: false     
+                            movie
                         })
                     })
+                }
+            })
+        }
+
+        getDirectorsAndActors = () => {
+        let endPoint = `${BASE_URL}/movie/${this.props.match.params.movieId}/credits?api_key=${API_KEY}`;
+        fetch(endPoint)
+            .then(response => response.json())
+            .then((credits) => {
+                console.log(credits)
+                const filterDirector = credits.crew.filter(person => person.job === "Director"); // filter directors from all employees
+                console.log(filterDirector)
+                this.setState({
+                    actors: credits.cast,
+                    directors: filterDirector[0].name,
+                    loader: false
                 })
-            }
-        })
+            })
     }
-  
+
     render() {
-        console.log(this.state.movie)
         return (
             <div>
-            <MovieInfo
-                movieInfo = {this.state.movie}
-                actors= {this.state.actors}
-                directors = {this.state.directors}
-            />
-            <Spinner />
+                <MovieInfo
+                    movieInfo={this.state.movie}
+                    actors={this.state.actors}
+                    directors={this.state.directors}
+                />
+                <Spinner />
             </div>
         )
     }
 }
 
 export default Movie;
+
+
+
+//! Previous Method
+// getMovieWithId = endPoint => {
+//     fetch(endPoint)
+//     .then(response => response.json())
+//     .then((movie) => {
+//         console.log(movie)
+
+//         if (movie.status_code) { // For error the request
+//             this.setState({
+//                 loader: false
+//             })
+//         }
+//         else { // İf success the request
+//             this.setState({ movie }, () => {
+//                 let endPoint = `${BASE_URL}/movie/${this.props.match.params.movieId}/credits?api_key=${API_KEY}`;
+//                 fetch(endPoint)
+//                 .then(response => response.json())
+//                 .then((credits) => {
+//                     console.log(credits)
+//                     const filterDirector = credits.crew.filter(person => person.job === "Director"); // filter directors from all employees
+//                     console.log(filterDirector)
+//                     this.setState({
+//                         actors: credits.cast,
+//                         directors: filterDirector[0].name,
+//                         loader: false     
+//                     })
+//                 })
+//             })
+//         }
+//     })
+// }
--- src/config.js
+++ src/config.js
@@ -4,7 +4,6 @@ const API_KEY = "368d4ab04a06cbb85c5a341e20d15411";
 const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
 const BACKDROP_SIZE = 'w1280';
 
-
 export {
     BASE_URL,
     BASE_IMG,

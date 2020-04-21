17dbaab97a3aedb2309b7c9dc52f0f3293e5ec36
Alican Uver <can.uverr@gmail.com>
2020-04-21 14:42:57 +0300

    baseUrl added

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ const BASE_IMG = "https://image.tmdb.org/t/p/w500";
 const API_KEY = "368d4ab04a06cbb85c5a341e20d15411";
 const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
 const BACKDROP_SIZE = 'w1280';
+const VIDEO_URL = "https://www.youtube.com/watch?v=";
 
 export {
     BASE_URL,
@@ -10,6 +11,7 @@ export {
     API_KEY,
     IMAGE_BASE_URL,
     BACKDROP_SIZE,
+    VIDEO_URL
 }
 
 

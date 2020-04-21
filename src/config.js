a8920435f64403cf77276f245d2f7c9cc2393644
Alican Uver <can.uverr@gmail.com>
2020-04-21 22:22:12 +0300

    youtube embed link added

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const BASE_IMG = "https://image.tmdb.org/t/p/w500";
 const API_KEY = "368d4ab04a06cbb85c5a341e20d15411";
 const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
 const BACKDROP_SIZE = 'w1280';
-const VIDEO_URL = "https://www.youtube.com/watch?v=";
+const VIDEO_URL = `https://www.youtube.com/embed/`;
 
 export {
     BASE_URL,
